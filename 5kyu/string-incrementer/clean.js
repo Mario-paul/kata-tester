376613f343201bdfbec9d502b356b9bc842b2a48
Mario P. Cardenas <15961362+Mario-paul@users.noreply.github.com>
2021-08-19 23:35:04 -0400

    string-incrementer: add clean.js kata version

---
+++ 5kyu/string-incrementer/clean.js
@@ -0,0 +1,24 @@
+// Version to be submitted to Codewars
+function incrementString(string) {
+  if (string === "") return "1";
+
+  var num = string;
+  const regex = new RegExp("(\\d+)", "gi");
+  let regMatch = num.match(regex);
+  if (regMatch === null) return string + 1;
+
+  const matchLength = regMatch[0].length;
+  let allNines = "9".repeat(matchLength);
+  
+  if (regMatch[0] === allNines) {
+    var result = num.replace(regex, (match) => {
+      return ("0".repeat(matchLength) + ++match).substr(-matchLength - 1);
+    });
+  } else {
+    var result = num.replace(regex, (match) => {
+      return ("0".repeat(matchLength) + ++match).substr(-matchLength);
+    });
+  }
+
+  return result;
+}
