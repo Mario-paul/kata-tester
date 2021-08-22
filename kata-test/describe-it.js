f70deb99033d72acd3812b17e46d881cebbe24aa
Mario P. Cardenas <15961362+Mario-paul@users.noreply.github.com>
2021-08-22 11:03:28 -0400

    kata-test: add indent feature to describe-it functions

--- kata-test/describe-it.js
+++ kata-test/describe-it.js
@@ -1,9 +1,15 @@
 function describe(string, func) {
+  console.group()
+  console.log(string)
   func();
+  console.groupEnd()
 }
 
 function it(string, func) {
+  console.group()
+  console.log(string)
   func()
+  console.groupEnd()
 }
 
 module.exports = {describe, it}
