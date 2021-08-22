e5a576693873e3ae75cac82b490c013020363a7d
Mario P. Cardenas <15961362+Mario-paul@users.noreply.github.com>
2021-08-22 00:27:16 -0400

    minor change

--- 6kyu/the-vowel-code/development.js
+++ 6kyu/the-vowel-code/development.js
@@ -9,18 +9,18 @@ const { describe, it } = require("../../kata-test/describe-it.js");    //
 // to execute code live on this IDE.
 
 function encode(string) {
-  return "h2ll4"
+  return "H4w 1r2 y45 t4d1y?";
 }
 
 function decode(string) {
-  return
+  return;
 }
 
-describe("Sample tests", function() {
-  it("Tests", function() {
-    assert.equal(encode('hello'), 'h2ll4');
-    assert.equal(encode('How are you today?'), 'H4w 1r2 y45 t4d1y?');
-    assert.equal(encode('This is an encoding test.'), 'Th3s 3s 1n 2nc4d3ng t2st.');
-    assert.equal(decode('h2ll4'), 'hello');
+describe("Sample tests", function () {
+  it("Tests", function () {
+    assert.equal(encode("hello"), "h2ll4");
+    assert.equal(encode("How are you today?"), "H4w 1r2 y45 t4d1y?");
+    assert.equal(encode("This is an encoding test."), "Th3s 3s 1n 2nc4d3ng t2st.");
+    assert.equal(decode("h2ll4"), "hello");
   });
 });
