9b3fabc5e3237579c393393cfff061b359a83265
Mario Cardenas <15961362+Mario-paul@users.noreply.github.com>
2021-08-25 23:57:14 -0400

    kata-test: Colors/safe branch merge. color output feature fully implemented (#13)
    * the-vowel-code: add terminal color output feature. currently broken
    
    * kata-test: color functionality fully implemented
    
    * minor change
    
    * kata-test: refactor code, add output spacers
    
    * minor change
    
    * kata-test: package.json license to MIT
    
    * kata-test: add indent feature to describe-it functions
    
    * describe-it minor edit
    
    * kata-test: condense kata-test.js output

--- kata-test/describe-it.js
+++ kata-test/describe-it.js
@@ -1,9 +1,18 @@
 function describe(string, func) {
+  console.log("Test Results:");
+  console.log("");
+
+  console.group();
+  console.log(string);
   func();
+  console.groupEnd();
 }
 
 function it(string, func) {
-  func()
+  console.group();
+  console.log(string);
+  func();
+  console.groupEnd();
 }
 
-module.exports = {describe, it}
+module.exports = { describe, it };
--- kata-test/kata-test.js
+++ kata-test/kata-test.js
@@ -1,21 +1,61 @@
+function moduleAvailable(name) {
+  try {
+    // console.log(`Checking if module '${name}' is installed...`);
+    // console.log(require.resolve(name)); // debug require.resolve()
+    require.resolve(name);
+    return true;
+  } catch (e) {}
+  console.log(
+    `Checking if module '${name}' is installed... Module '${name}' was NOT found. Please run "npm install" inside ./kata-test directory to install.`
+  );
+  console.log("");
+  return false;
+}
+
+let colors;
+let colorsLoaded = false;
+
+if (moduleAvailable("colors")) {
+  // yeah we've got it!
+  colors = require("colors/safe");
+  // let colors = require("colors/safe")
+  console.log(`Checking if module 'colors' is installed... 'colors' is installed!`);
+  console.log(``);
+  colorsLoaded = true;
+}
+
 module.exports = class Test {
-  assertEquals(input, expectedOutput) {
+  testLogic(input, expectedOutput) {
+    // console.log("colors loaded: ", colorsLoaded); //debug colors module
+    const spacerPassed = "--------------";
+    const spacerFailed = "---------------------------------------------";
     if (input === expectedOutput) {
       // return true
-      console.log("🗸 Test Passed");
+      if (colorsLoaded) {
+        console.log(colors.green("🗸 Test Passed"));
+      } else {
+        console.log("🗸 Test Passed");
+      }
+      console.log(spacerPassed);
     } else {
       // return false
-      console.log(`✗ expected ${input} to equal ${expectedOutput}`);
+      if (colorsLoaded) {
+        console.log(
+          colors.red(`✗ expected ${input} to equal ${expectedOutput}`)
+        );
+      } else {
+        console.log(`✗ expected ${input} to equal ${expectedOutput}`);
+      }
+      console.log(spacerFailed);
+      // process.exit(1) // Codewars-like behavior, exit when a test fails
     }
   }
 
+  assertEquals(input, expectedOutput) {
+    this.testLogic(input, expectedOutput);
+  }
+
   equal(input, expectedOutput) {
-    if (input === expectedOutput) {
-      // return true
-      console.log("🗸 Test Passed");
-    } else {
-      // return false
-      console.log(`✗ expected ${input} to equal ${expectedOutput}`);
-    }
+    this.testLogic(input, expectedOutput);
   }
 };
