d931cc9a1d64f27c6fc8a70139a9e2e55d41526e
Mario P. Cardenas <15961362+Mario-paul@users.noreply.github.com>
2021-08-31 00:30:08 -0400

    the-vowel-code: clean up .js files

--- 6kyu/the-vowel-code/clean.js
+++ 6kyu/the-vowel-code/clean.js
@@ -1,6 +1,9 @@
 // Version to be submitted to Codewars
 
-function kata() {
-  // Your code here...
+function encode(string) {
+  return "H4w 1r2 y45 t4d1y?";
+}
+
+function decode(string) {
   return;
 }
