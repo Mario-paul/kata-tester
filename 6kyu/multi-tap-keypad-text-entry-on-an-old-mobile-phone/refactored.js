4c7bc7bc6cbf58c7d06688a8e8bddea39bb5560b
Mario P. Cardenas <15961362+Mario-paul@users.noreply.github.com>
2021-08-31 00:17:49 -0400

    multi-pad: clean .js files

--- 6kyu/multi-tap-keypad-text-entry-on-an-old-mobile-phone/refactored.js
+++ 6kyu/multi-tap-keypad-text-entry-on-an-old-mobile-phone/refactored.js
@@ -1,10 +1,3 @@
-/***********************************************************************/
-const KataTest = require("../../kata-test/kata-test.js");
-const Test = new KataTest();
-const { describe, it } = require("../../kata-test/describe-it.js");
-/***********************************************************************/
-/* Environment setup. Modify only as needed.                           */
-
 // Refactored kata. One liners are ideal, though not necessary.
 // Test your code here! Use Quokka Pro or Code Runner to execute code
 // live on this IDE.
@@ -13,10 +6,3 @@ function presses(phrase) {
   // Your code here...
   return;
 }
-
-describe("Tests", () => {
-  it("test", () => {
-Test.assertEquals(presses("LOL"), 9, "should work for simple examples")
-Test.assertEquals(presses("HOW R U"), 13, "should work for phrases with spaces")
-  });
-});
