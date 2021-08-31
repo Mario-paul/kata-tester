8122a6516f55342995c60a0f21812d12c2d4003e
Mario P. Cardenas <15961362+Mario-paul@users.noreply.github.com>
2021-08-31 00:15:44 -0400

    kata-template: simplyfy .js files, made all but kata.js optional

--- kata-template/refactored.js
+++ kata-template/refactored.js
@@ -1,25 +1,7 @@
-/***********************************************************************/
-const KataTest = require("../../kata-test/kata-test.js");
-const Test = new KataTest();
-const { describe, it } = require("../../kata-test/describe-it.js");
-/***********************************************************************/
-/* Environment setup. Modify only as needed.                           */
-
 // Refactored kata. One liners are ideal, though not necessary.
-// Test your code here! Use Quokka Pro or Code Runner to execute code
-// live on this IDE.
+// Test your code here! Use Code Runner to execute code live on this IDE.
 
 function kata() {
   // Your code here...
   return;
 }
-
-describe("Tests", () => {
-  it("test", () => {
-    Test.assertEquals(kata("foobar000"), "foobar001");
-    Test.assertEquals(kata("foo"), "foo1");
-    // Replace these Sample Tests with the ones provided by your kata!
-    // Make sure to replace variable on line 3 (Test) with the class name
-    // in the Sample Tests you paste here.
-  });
-});
